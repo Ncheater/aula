ca787e6927757a731ee783396c4b2a435347e99e
KuuHaKu <yagogimenez1@hotmail.com>
2017-10-31 19:49:50 -0200

    Delete javascript.js

--- js/javascript.js
+++
@@ -1,9 +0,0 @@
-function funcaosomar(){
-	var n1 = document.getElementById("input1");
-	var n2 = document.getElementById("input2");
-	var sum;
-	
-	sum = parseFloat(n1.value) + parseFloat(n2.value);
-	
-	document.getElementById("resultado").innerHTML = sum;
-}
